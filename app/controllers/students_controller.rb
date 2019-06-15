5228b518b49c43026ebfda764f7825c463f823cf
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-15 22:39:00 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :edit, :update, :activate, :destroy]
   
   def index
     @students = Student.all
@@ -8,9 +8,33 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def edit 
+     @submit_text = "Fix This Student"
+  end 
+
+  def update 
+      if @student.valid?
+      @student.update(student_params)
+      redirect_to student_path(@student) 
+    else
+      render :edit
+    end
+  end 
+
+  def activate 
+    @student.active = !@student.active
+    @student.save 
+    redirect_to student_path(@student.id)
+  end  
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
+    def student_params
+      params.require(:student).permit(:id, :first_name, :last_name, :active)
+    end 
+
 end
